c2a5499588a06c558007fbb9d7b836a2eda3ab1b
Bhanu-dev1996 <113974500+Bhanu-dev1996@users.noreply.github.com>
2025-04-11 20:51:51 +0530

    project data changes

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -8,8 +8,8 @@ export const projectsData = [
         description: "The Note-Taking App is a simple and intuitive web-based tool that allows users to quickly jot down, save, and manage their notes.",
         tags: ["HTML", "CSS", "JavaScript"],
         image: NoteTakingApp,
-        demoLink: "https://note-app-demo.example.com",
-        codeLink: "https://github.com/yourusername/note-taking-app"
+        demoLink: "https://notepad-lite.netlify.app/",
+        codeLink: "https://github.com/Bhanu-dev1996/note-taking-app"
     },
     
 ];
